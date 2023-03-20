bbade0c3585c7c87b61622650efc7c20bb2ac4a3
Ali Khan <alik@robarts.ca>
2023-03-20 09:21:57 -0400

    linting

--- diffparc/workflow/scripts/seg_to_tissue.py
+++ diffparc/workflow/scripts/seg_to_tissue.py
@@ -10,16 +10,14 @@ dseg_nib = nib.load(snakemake.input.dseg)
 dseg_vol = dseg_nib.get_fdata()
 tissue_vol = np.zeros(dseg_vol.shape)
 
-#extract tissue label
+# extract tissue label
 for tissue_label_name, tissue_label_num in tissue_lut.items():
 
-    labels = df.loc[df['tissue'] == tissue_label_name].label 
+    labels = df.loc[df["tissue"] == tissue_label_name].label
 
     for label in labels:
-        tissue_vol[dseg_vol==label] = tissue_label_num
+        tissue_vol[dseg_vol == label] = tissue_label_num
 
-#save the tissue dseg file
-out_nib = nib.Nifti1Image(tissue_vol,affine=dseg_nib.affine,header=dseg_nib.header)
+# save the tissue dseg file
+out_nib = nib.Nifti1Image(tissue_vol, affine=dseg_nib.affine, header=dseg_nib.header)
 out_nib.to_filename(snakemake.output.dseg)
-
-
